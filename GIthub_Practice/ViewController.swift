8cbffca6f2a411d0463237d70f453172ea6fb173
jasmeen <503jasmeen@gmail.com>
2021-08-14 13:52:49 -0400

    First Commit

--- GIthub_Practice/ViewController.swift
+++ GIthub_Practice/ViewController.swift
@@ -11,6 +11,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        print("Jasmeen Kaur waheguru g")
         // Do any additional setup after loading the view.
     }
 
