576299fc296de96028b441c8aff841089b42a0d7
jasmeen <503jasmeen@gmail.com>
2021-08-14 12:20:08 -0400

    Initial Commit

---
+++ GIthub_Practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GIthub_Practice
+//
+//  Created by Jasmeen Kular on 2021-08-14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
